a5025f1195a56b1b3099ef7bb36102d12efb9354
CEM ŞAHİN <cmshn02@gmail.com>
2022-06-20 21:16:59 +0300

    64.Ders Commit Edildi

---
+++ BusinessLayer/ValidationRules/ContactValidator.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+  public  class ContactValidator : AbstractValidator<Contact>
+    {
+        public ContactValidator()
+        {
+            RuleFor(x => x.UserMail).NotEmpty().WithMessage("Mail Adresini Boş Geçmeyiniz");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konu Adını Boş geçmeyeniz")
+             .MinimumLength(3).WithMessage("Lütfen en az 3 karakter girişi yapınız")
+             .MaximumLength(20).WithMessage("Lütfen 50 karakterden fazla veri girişi yapmayınız");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Açıklamayı boş geçmeyiniz").MinimumLength(3).WithMessage("Lütfen en az 3 karakter giriniz.");
+
+        }
+
+
+    }
+}
---
+++ BusinessLayer/ValidationRules/MessageValidator.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class MessageValidator : AbstractValidator<Message>
+    {
+        public MessageValidator()
+        {
+
+            RuleFor(x => x.ReceiverMail).NotEmpty().WithMessage("Alıcı Mail Adresini Boş Geçmeyiniz").EmailAddress().WithMessage("Geçerli bir e mail adresi giriniz.");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konu Adını Boş geçmeyeniz")
+                 .MinimumLength(3).WithMessage("Lütfen en az 3 karakter girişi yapınız")
+                 .MaximumLength(20).WithMessage("Lütfen 50 karakterden fazla veri girişi yapmayınız");
+            RuleFor(x => x.MessageContent).NotEmpty().WithMessage("Açıklamayı boş geçmeyiniz").MinimumLength(10).WithMessage("Lütfen en az 10 karakter giriniz.");
+
+
+
+        }
+
+    }
+}
