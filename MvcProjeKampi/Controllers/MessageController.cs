522780a01624f351e6d2d6ca547c4ebb8db41956
CEM ŞAHİN <cmshn02@gmail.com>
2022-06-25 13:55:49 +0300

    66.Ders Commit edildi

--- MvcProjeKampi/Controllers/MessageController.cs
+++ MvcProjeKampi/Controllers/MessageController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +14,10 @@ namespace MvcProjeKampi.Controllers
     public class MessageController : Controller
     {
         // GET: Message
-
-
         MessageManager mm = new MessageManager(new EfMessageDal());
+        MessageValidator messagevalidator = new MessageValidator();
+
+      
         public ActionResult Inbox()
         {
 
@@ -30,6 +33,18 @@ namespace MvcProjeKampi.Controllers
             var messagelist = mm.GetListSendbox();
          return View(messagelist);
         }
+        public ActionResult GetInboxMessageDetails(int id)
+        {
+            var values = mm.GetByID(id);
+            return View(values);
+        }
+        public ActionResult GetSendboxMessageDetails(int id)
+        {
+            var values = mm.GetByID(id);
+            return View(values);
+        }
+
+
         [HttpGet]
         public ActionResult NewMessage()
         {
@@ -38,7 +53,25 @@ namespace MvcProjeKampi.Controllers
         [HttpPost]
         public ActionResult NewMessage(Message p)
         {
+
+
+            ValidationResult results = messagevalidator.Validate(p);
+
+            if (results.IsValid)
+            {
+                p.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                mm.MessageAdd(p);
+                return RedirectToAction("SendBox");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
+
         }
     }
 }
