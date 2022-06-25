101cd8b679a9a23c55f4b87597ec01b925c4769d
CEM ŞAHİN <cmshn02@gmail.com>
2022-06-26 01:24:05 +0300

    70.Ders Commit edildi

---
+++ MvcProjeKampi/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admın p)
+        {
+
+            Context c = new Context();
+            var admınUserinfo = c.Admins.FirstOrDefault(x => x.AdminUserName ==p.AdminUserName && x.AdminPassword==p.AdminPassword);
+            if (admınUserinfo!=null)
+            {
+
+                return RedirectToAction("Index","AdminCategory");
+
+
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
+
+        }
+    }
+}
